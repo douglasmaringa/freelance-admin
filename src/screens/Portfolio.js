7f42c4d50e4efbf00ae6842664f9a57f9cead151
Douglas Maringa <douglasmaringa4@gmail.com>
2022-03-04 11:14:32 +0200

    Update Portfolio.js

--- src/screens/Portfolio.js
+++ src/screens/Portfolio.js
@@ -267,7 +267,7 @@ function Portfolio() {
                 
             </div>
         </div>
-        <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" style={{marginLeft:"30px"}} onClick={open}>Add New Member</button>
+        <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" style={{marginLeft:"30px"}} onClick={open}>Add New Project to Your Portfolio</button>
     </div>
 </div>
 
